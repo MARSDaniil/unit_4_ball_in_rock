5de04bc804bd284ccf386385869bd396daac88f7
MARS <61511540+MARSMPEI@users.noreply.github.com>
2022-10-16 01:02:07 +0300

    add other ball, which attack u, add generate of attackers ball

---
+++ Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject enemyPrefab;
+    private float spawnRange = 9;
+    void Start()
+    {
+        Vector3 randomPosition = GenerateSpawnPosition();
+        Instantiate(enemyPrefab, randomPosition, enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private Vector3 GenerateSpawnPosition()
+    {
+
+        float spawnPositionX = Random.Range(-spawnRange, spawnRange);
+        float spawnPositionZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPosition = new Vector3(spawnPositionX, 0, spawnPositionZ);
+
+        return (randomPosition);
+    }
+}
